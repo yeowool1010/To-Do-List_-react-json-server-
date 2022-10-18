eea513066133d3525083d9b7734feb11ba1b6123
yeowool1010 <qyoong3579@gmail.com>
2022-10-18 17:36:47 +0900

    Updates

---
+++ precache-manifest.a9afb5ada93ec05651cd1198c0dd8f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08490e6293a7b9f849c8",
+    "url": "/To-Do-List_-react-json-server-/static/css/main.c2b723ad.chunk.css"
+  },
+  {
+    "revision": "08490e6293a7b9f849c8",
+    "url": "/To-Do-List_-react-json-server-/static/js/main.9454036b.chunk.js"
+  },
+  {
+    "revision": "c49b419dd59c198ec380",
+    "url": "/To-Do-List_-react-json-server-/static/js/runtime~main.23b8b357.js"
+  },
+  {
+    "revision": "46ed4cdb50631b24b69f",
+    "url": "/To-Do-List_-react-json-server-/static/js/2.02487657.chunk.js"
+  },
+  {
+    "revision": "b0fc5f8c20839e372e06d83d49537d22",
+    "url": "/To-Do-List_-react-json-server-/index.html"
+  }
+];
